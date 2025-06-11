138f5b781df7664923271e8a6a7a033bf0436e6b
dkaneshi <dkaneshi@gmail.com>
2025-06-10 21:22:35 -1000

    feat: add relationships and properties to Organization model

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read Team $teams
+ * @property-read SignInSheet $signInSheets
+ * @property-read CarbonImmutable $created_at
+ * @property-read CarbonImmutable $updated_at
+ */
 class Organization extends Model
 {
     /** @use HasFactory<\Database\Factories\OrganizationFactory> */
     use HasFactory;
+
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function signInSheets(): HasMany
+    {
+        return $this->hasMany(SignInSheet::class);
+    }
+
 }
