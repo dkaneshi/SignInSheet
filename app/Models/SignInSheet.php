6911d1795d0a5148f35800715f3c360b963623ca
dkaneshi <dkaneshi@gmail.com>
2025-06-10 21:22:55 -1000

    feat: add relationships and properties to SignInSheet model

--- app/Models/SignInSheet.php
+++ app/Models/SignInSheet.php
@@ -2,11 +2,41 @@
 
 namespace App\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property-read int $id
+ * @property-read int $organization_id
+ * @property-read Organization $organization
+ * @property-read int $team_id
+ * @property-read Team $team
+ * @property-read SignInEntry $signInEntries
+ * @property-read CarbonImmutable $start_date
+ * @property-read CarbonImmutable $end_date
+ * @property-read CarbonImmutable $created_at
+ * @property-read CarbonImmutable $updated_at
+ */
 class SignInSheet extends Model
 {
     /** @use HasFactory<\Database\Factories\SignInSheetFactory> */
     use HasFactory;
+
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function signInEntries(): HasMany
+    {
+        return $this->hasMany(SignInEntry::class);
+    }
 }
