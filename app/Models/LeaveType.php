2382e198a98e5527d9f4a28dbb7bc80567bde4a6
dkaneshi <dkaneshi@gmail.com>
2025-06-10 21:22:21 -1000

    feat: add factory and relationship to LeaveType model

--- app/Models/LeaveType.php
+++ app/Models/LeaveType.php
@@ -3,16 +3,26 @@
 namespace App\Models;
 
 use Carbon\CarbonImmutable;
+use Database\Factories\LeaveTypeFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property-read integer $id
  * @property-read string $name
  * @property-read string $abbreviation
+ * @property-read SignInEntry $signInEntries
  * @property-read CarbonImmutable $created_at
  * @property-read CarbonImmutable $updated_at
  */
 class LeaveType extends Model
 {
-    //
+    /** @use HasFactory<LeaveTypeFactory> */
+    use HasFactory;
+
+    public function signInEntries(): HasMany
+    {
+        return $this->hasMany(SignInEntry::class);
+    }
 }
